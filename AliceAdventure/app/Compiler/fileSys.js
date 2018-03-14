90e774fe64c33d5ad20d6c282f629a0e039f6037
thriller6767 <thriller6767@gmail.com>
2018-03-13 21:01:04 -0400

    a bug when parsing json

--- AliceAdventure/app/Compiler/fileSys.js
+++ AliceAdventure/app/Compiler/fileSys.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs-extra');
+const path = require('path');
 
 //1) create a build folder. If it already exists, delete all the files within
 //2) copy assets folder to build folder
@@ -34,23 +35,48 @@ FileSys.writeFile = function(dest, string){
 	fs.outputFileSync(dest, string);
 }
 
+
+FileSys.merge = function(p1,p2){
+	return path.join(p1, p2);
+}
+
 //need to create a html basic file
 //need to rename the title
 
-FileSys.ensureAndCreate = function(){
-	var buildPath = '../build';
-	var assetSrc = '../assets';
-	var assetDest = '../build/assets';
-	var aliceAPISrc = '../aliceAPI.js';
-	var aliceAPIDest = '../build/aliceAPI.js';
-	var pixiSrc = '../pixi';
-	var pixiDest = '../build/pixi';
+
+//return path to the resources folder under build path
+FileSys.ensureAndCreate = function(jsonPath, callback){
+
+	jsonPath = path.dirname(jsonPath);
+	var buildPath = path.join(jsonPath, 'Build');
+	var resourcesDest = path.join(buildPath, 'Resources');
+	//var assetSrc = path.join(jasonPath, 'assets');
+	var assetDest = path.join(resourcesDest, 'Assets');
+	var aliceAPISrc = '../Engine/aliceAPI.js';
+	var aliceAPIDest = path.join(resourcesDest, 'aliceAPI.js');
+	var pixiSrc = '../Resources/pixi.js';
+	var pixiDest = path.join(resourcesDest, 'pixi.js');
+
+	// if (! fs.pathExistSync(assetSrc)){
+	// 	callback("Cannot find the assets folder under saving directory.");
+	// 	return false;
+	// }
+	if (! fs.pathExistsSync(aliceAPISrc)){
+		callback("Cannot find aliceAPI.js, which should be under Engine/aliceAPI.js");
+		return false;
+	}
+	if (! fs.pathExistsSync(pixiSrc)){
+		callback("Cannot find pixi.js, which should be under Resources/pixi.js");
+		return false;
+	}
 
 	FileSys.createBuildFolder(buildPath);
-	FileSys.copyFileOrFolder(assetSrc, assetDest);
+	FileSys.createBuildFolder(resourcesDest);
+	FileSys.createBuildFolder(assetDest);
 	FileSys.copyFileOrFolder(aliceAPISrc, aliceAPIDest);
 	FileSys.copyFileOrFolder(pixiSrc, pixiDest);
 
+	return buildPath;
 }
 
 module.exports = FileSys;
