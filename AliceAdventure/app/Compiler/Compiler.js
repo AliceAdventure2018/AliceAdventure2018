2527ea7bfb4cc8b49a3c1ef949d80512ffeac049
Ruili Tang <ruilit@andrew.ad.cmu.edu>
2018-03-16 15:28:19 -0400

    compiler export module

--- AliceAdventure/app/Compiler/Compiler.js
+++ AliceAdventure/app/Compiler/Compiler.js
@@ -1,18 +1,19 @@
 
+'use strict';
+
 const FileSys = require('./FileSys.js');
 const Parser = require('./SimpleParser.js');
 
-function fileSysAlert(e){
-	console.log(e);
-}
+var  Compiler;
 
-function compile(path){
-	var buildDest = FileSys.ensureAndCreate(path,fileSysAlert);
-	var parser = new Parser(path,buildDest);
+Compiler = function (path, callback){
+	this.buildDest = FileSys.ensureAndCreate(path,callback);
+	this.parser = new Parser(path,buildDest);
 	
-	var string = parser.translate(fileSysAlert);
-
-	FileSys.writeFile(FileSys.merge(buildDest, 'game.js'), string);
+	this.build = function(callback){
+		var string = this.parser.translate(callback);
+		FileSys.writeFile(FileSys.merge(this.buildDest, 'game.js'), string);
+	}
 }
 
-compile("C:/Users/ruilit/Documents/2018-SP/Alice/test.json");
+module.exports = Compiler;
