b5fbad21666343d43fb60666993115bdde0427ca
Tianyi Zhao <tz1@andrew.cmu.edu>
2018-03-28 21:42:40 -0400

    FOR HALVES!

--- AliceAdventure/app/Editor/Scripts/EditorScripts/IReaction.js
+++ AliceAdventure/app/Editor/Scripts/EditorScripts/IReaction.js
@@ -27,55 +27,78 @@ IReaction.Library = [
 		type: 0,
 		name: "Set state", 
 		inputLength: 2, 
-		inputTypes: [IReaction.InputModel.State, IReaction.InputModel.Checkbox]
+		inputTypes: [IReaction.InputModel.State, IReaction.InputModel.Checkbox], 
+		template: "Set state <select><option selected>win</option></select> to <input type='checkbox' value='true'>"
 	}, 
 	{
 		type: 1,
 		name: "Transit to scene", 
 		inputLength: 1, 
-		inputTypes: [IReaction.InputModel.Scene]
+		inputTypes: [IReaction.InputModel.Scene],
+		template: "Transit to scene <select><option selected>Kitchen</option><option>Living room</option><option>Win scene</option></select>"
 	}, 
 	{
 		type: 2,
-		name: "Store in inventory", 
+		name: "Put into inventory", 
 		inputLength: 1, 
-		inputTypes: [IReaction.InputModel.Object]
+		inputTypes: [IReaction.InputModel.Object],
+		template: "Put <select><option selected>Knife</option><option>Jam</option><option>Knife with jam</option><option>Bread with jam</option><option>Bread</option><option>Cat</option><option>Door</option></select> into inventory"
 	}, 
 	{
 		type: 3,
 		name: "Remove out of inventory", 
 		inputLength: 1, 
-		inputTypes: [IReaction.InputModel.Object]
+		inputTypes: [IReaction.InputModel.Object],
+		template: "Remove out of inventory"
 	}, 
 	{
 		type: 4,
-		name: "Set active", 
+		name: "Make visible", 
 		inputLength: 2, 
-		inputTypes: [IReaction.InputModel.Object, IReaction.InputModel.Checkbox]
+		inputTypes: [IReaction.InputModel.Object, IReaction.InputModel.Checkbox],
+		template: "Make <select><option selected>Knife</option><option>Jam</option><option>Knife with jam</option><option>Bread with jam</option><option>Bread</option><option>Cat</option><option>Door</option></select> visible"
+	}, 
+	{
+		type: 4.5,
+		name: "Make invisible", 
+		inputLength: 2, 
+		inputTypes: [IReaction.InputModel.Object, IReaction.InputModel.Checkbox],
+		template: "Make <select><option selected>Knife</option><option>Jam</option><option>Knife with jam</option><option>Bread with jam</option><option>Bread</option><option>Cat</option><option>Door</option></select> invisible"
 	}, 
 	{
 		type: 5,
-		name: "Set interactive", 
+		name: "Make interactive", 
 		inputLength: 2, 
-		inputTypes: [IReaction.InputModel.Object, IReaction.InputModel.Checkbox]
+		inputTypes: [IReaction.InputModel.Object, IReaction.InputModel.Checkbox],
+		template: "Make interactive"
 	}, 
 	{
+		type: 5.5,
+		name: "Make non-interactive", 
+		inputLength: 2, 
+		inputTypes: [IReaction.InputModel.Object, IReaction.InputModel.Checkbox],
+		template: "Make non-interactive"
+	}, 
+	/*{
 		type: 6,
 		name: "Play audio", 
 		inputLength: 1, 
-		inputTypes: [IReaction.InputModel.Audio]
+		inputTypes: [IReaction.InputModel.Audio],
+		template: "Play audio"
 	}, 
 	{
 		type: 7,
 		name: "Play animation", 
 		inputLength: 1, 
-		inputTypes: [IReaction.InputModel.Animation]
-	}, 
+		inputTypes: [IReaction.InputModel.Animation],
+		template: "Play animation"
+	}, */
 	{
 		type: 8,
 		name: "Show textbox", 
 		inputLength: 1, 
-		inputTypes: [IReaction.InputModel.Textbox]
+		inputTypes: [IReaction.InputModel.Textbox],
+		template: "Show textbox: <input type='textbox'>"
 	}
 ];
 
