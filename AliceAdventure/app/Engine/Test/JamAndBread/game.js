aa2c3113341b327780143fab950a237076f42de0
meow.ren9 <meow.ren9@gmail.com>
2018-04-04 22:18:59 -0400

    Engine: message box scale

--- AliceAdventure/app/Engine/Test/JamAndBread/game.js
+++ AliceAdventure/app/Engine/Test/JamAndBread/game.js
@@ -1,7 +1,7 @@
 var myGame = new GameManager();
-myGame.init(1280,720,8);
+//myGame.init(1280,720,8);
 //myGame.init(600,400,5);
-//myGame.init(640,360,5);
+myGame.init(1280*3/4,720*3/4,5);
 myGame.sceneManager.createScenes(3);
 myGame.states = {cat_is_feeded:false}
 myGame.initStateManager({cat_is_feeded:false});
