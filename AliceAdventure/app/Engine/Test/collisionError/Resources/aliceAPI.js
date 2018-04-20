9ef23f14bc552e1b43afcd2c66dc2f3b0e4a434c
meow.ren9 <meow.ren9@gmail.com>
2018-04-19 23:46:41 -0400

    Engine: API renew

--- AliceAdventure/app/Engine/Test/collisionError/Resources/aliceAPI.js
+++ AliceAdventure/app/Engine/Test/collisionError/Resources/aliceAPI.js
@@ -348,7 +348,7 @@ function Inventory(game) { //always on the top
             var message = tool.name + this.game.eventSystem.template.combine + inventoryCollider.pop().name;
             if(this.game.eventSystem.checkEventExist(message)){
                 //game.sound.play('good');
-                //this.game.soundManager.play('good');
+                this.game.soundManager.play('good');
                 this.game.eventSystem.callEvent(message);
                 return;
             }
@@ -359,7 +359,7 @@ function Inventory(game) { //always on the top
             //console.log(message);
             if(this.game.eventSystem.checkEventExist(message)){
                 //game.sound.play('good');
-                //this.game.soundManager.play('good');
+                this.game.soundManager.play('good');
                 this.game.eventSystem.callEvent(message);
                 return;
             }
@@ -1025,6 +1025,7 @@ function onMouseUp() {
             var item = sceneCollider.pop();
             var message = this.name + myGame.eventSystem.template.use + item.name;
             //console.log(message);
+            console.log("use msg: " + message)
             if(myGame.eventSystem.checkEventExist(message)){
                 myGame.soundManager.play('good');
                 myGame.eventSystem.callEvent(message);
@@ -1033,7 +1034,7 @@ function onMouseUp() {
             
             message = this.name + myGame.eventSystem.template.combine + item.name;
             
-            //console.log("combine msg: " + message)
+            console.log("combine msg: " + message)
             if(myGame.eventSystem.checkEventExist(message)){
                 myGame.soundManager.play('good');
                 myGame.eventSystem.callEvent(message);
